3be336fc6a20247df3934aa0956426c6f9f9fdf4
CLEMENT ARTHUR p2301438 <arthur.clement@etu.univ-lyon1.fr>
2024-10-22 09:08:14 +0200

    fix v2 final final v4

--- login.php
+++ login.php
@@ -1,18 +1,16 @@
 <?php
 require_once "includes/headerfunction.php";
-require_once "includes/user_existe.php";
 require_once "includes/header.php";
+require_once "includes/realheader.php";
+require_once "includes/nav.php";
 ?>
-<link rel="stylesheet" href="res/css/styleform.css">
 <form action="login.php" method="post">
-    <div class=" formulary">
     <label>Email : </label>
-    <input name="email" id="e_mail" type="email" placeholder="e-mail"/><br>
+    <input name="email" type="email" placeholder="email"/><br>
 
     <label>Mot de passe : </label>
     <input name="password" id="password" type="text" /><br>
     <button type="submit">Valider</button>
-    </div>
 </form>
 
 <?php
@@ -35,4 +33,8 @@ if (!empty($_POST["email"]) && !empty($_POST["password"])) {
     header('Location: account.php');
 }
 
+?>
+
+<?php
+require_once "includes/footer.php";
 ?>
